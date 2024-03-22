9bce765637a99ae7e1e5e170e33aca4038a679a8
weibdev <153146455+weibdev@users.noreply.github.com>
2024-03-21 21:26:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 const path = require("path") 
+var cors = require('cors')
+
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, "static")))
 
